7156e7f3ae245990c421d8cdc603e5f2b0b3b018
Evan Palmer <evansp01@gmail.com>
2015-03-25 12:39:37 -0400

    added many base html files

--- everest/urls.py
+++ everest/urls.py
@@ -2,9 +2,18 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('everest.views',
     url(r'^$', 'general.home',name=''),
+
+
     # Route for built-in authentication with our own custom login page
 #    url(r'^register$', 'register'),
 #    url(r'^confirm/(?P<username>[a-zA-Z0-9_\-\.]*)/(?P<token>[a-z0-9\-]+)$','confirm'),
+)
+
+urlpatterns += patterns('everst.views.general',
+    url(r'^fild_list$','general.find_list',name='find_list'),
+    url(r'^fild_user$','general.find_user',name='find_user'),
+    url(r'^fild_sentence$','general.find_sentence',name='find_sentence'),
+    url(r'^manage_account$','general.manage_account',name='manage_account')
 )
 
 # urlpatterns += patterns('',
