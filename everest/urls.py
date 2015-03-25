2dc43368ab8c18f1e29db7b0c34d1478858e7c0b
Evan Palmer <evansp01@gmail.com>
2015-03-25 12:49:14 -0400

    added register function

--- everest/urls.py
+++ everest/urls.py
@@ -2,22 +2,35 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('everest.views',
     url(r'^$', 'general.home',name=''),
-
-
     # Route for built-in authentication with our own custom login page
 #    url(r'^register$', 'register'),
 #    url(r'^confirm/(?P<username>[a-zA-Z0-9_\-\.]*)/(?P<token>[a-z0-9\-]+)$','confirm'),
 )
 
-urlpatterns += patterns('everst.views.general',
-    url(r'^fild_list$','general.find_list',name='find_list'),
-    url(r'^fild_user$','general.find_user',name='find_user'),
-    url(r'^fild_sentence$','general.find_sentence',name='find_sentence'),
-    url(r'^manage_account$','general.manage_account',name='manage_account')
+urlpatterns += patterns('everest.views.general',
+    url(r'^fild_list$','find_list',name='find_list'),
+    url(r'^fild_user$','find_user',name='find_user'),
+    url(r'^fild_sentence$','find_sentence',name='find_sentence'),
+    url(r'^manage_account$','manage_account',name='manage_account')
 )
 
-# urlpatterns += patterns('',
-#     url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'everest/login.html'}),
-#     # Route to logout a user and send them back to the login page
-#     url(r'^logout$', 'django.contrib.auth.views.logout_then_login'),
-#     )
+
+# urlpatterns += patterns('everst.views.teacher',
+#     url(r'^fild_list$','general.find_list',name='find_list'),
+#     url(r'^fild_user$','general.find_user',name='find_user'),
+#     url(r'^fild_sentence$','general.find_sentence',name='find_sentence'),
+#     url(r'^manage_account$','general.manage_account',name='manage_account')
+# )
+
+# urlpatterns += patterns('everst.views.translator',
+#     url(r'^fild_list$','general.find_list',name='find_list'),
+#     url(r'^fild_user$','general.find_user',name='find_user'),
+#     url(r'^fild_sentence$','general.find_sentence',name='find_sentence'),
+#     url(r'^manage_account$','general.manage_account',name='manage_account')
+# )
+
+urlpatterns += patterns('',
+    url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'everest/login.html'}),
+    # Route to logout a user and send them back to the login page
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login'),
+    )
