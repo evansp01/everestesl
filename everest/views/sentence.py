a62e3dfd14c838a85a45f2c12b890a29548f169f
Evan Palmer <evansp01@gmail.com>
2015-04-07 05:28:28 -0400

    moved record to sentence view

--- everest/views/sentence.py
+++ everest/views/sentence.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 import json
+from everest.models import *
 
 
 def home(request):
@@ -18,3 +20,9 @@ def upload(request):
         pass
     to_json = {'yes': "hi"}
     return HttpResponse(json.dumps(to_json), content_type='application/json')
+
+def view_sentence(request, sentence):
+    sentence = get_object_or_404(Sentence, id=sentence)
+    context = {'sentence': sentence}
+    return render(request, 'everest/sentence.html', context)
+
