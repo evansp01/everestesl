6d3c39d570fbb485910accf69448e531fb741d58
Marjorie Carlson <marjorie@cmu.edu>
2015-04-10 14:20:25 -0400

    Created del fns in URLs and delete.py
    Haven’t filled the rest out yet tho

--- everest/views/delete.py
+++ everest/views/delete.py
@@ -18,3 +18,28 @@ def del_translation(request, translation):
     if request.method == 'POST' and request.user == translation.creator:
         translation.delete()
     return render(request, 'everest/sentence.html', context)
+
+
+@login_required
+@transaction.atomic
+def del_englishaudio(request, englishaudio):
+    context = {'sentence': englishaudio.sentence}
+    return render(request, 'everest/sentence.html', context)
+
+@login_required
+@transaction.atomic
+def del_nepaliaudio(request, nepaliaudio):
+    context = {'sentence': nepaliaudio.sentence}
+    return render(request, 'everest/sentence.html', context)
+
+@login_required
+@transaction.atomic
+def del_sentence(request, sentence):
+    context = {'lesson': sentence.lesson}
+    return render(request, 'everest/sentence.html', context)
+
+@login_required
+@transaction.atomic
+def del_lesson(request, lesson):
+    context = {'lesson': lesson}
+    return render(request, 'everest/sentence.html', context)
