69c294b52ed7486fcb48c6c87719c1debaa6bfac
Marjorie Carlson <marjorie@cmu.edu>
2015-04-10 14:47:38 -0400

    Deletions call cleanup (not yet written)
    del_sentence, del_lesson, cleanup all need to be written

--- everest/views/delete.py
+++ everest/views/delete.py
@@ -14,37 +14,51 @@ from everest.forms import *
 @transaction.atomic
 def del_translation(request, translation):
     translation = get_object_or_404(Translation, id=translation)
-    context = {'sentence': translation.sentence}
+    sentence = translation.sentence
     if request.method == 'POST' and request.user == translation.creator:
         translation.delete()
+        cleanup(sentence)
+    context = {'sentence': sentence}
     return render(request, 'everest/sentence.html', context)
 
 @login_required
 @transaction.atomic
 def del_englishaudio(request, audio):
     audio = get_object_or_404(EnglishAudio, id=audio)
-    context = {'sentence': audio.sentence}
+    sentence = audio.sentence
     if request.method == 'POST' and request.user == audio.creator:
         audio.delete()
+        cleanup(sentence)
+    context = {'sentence': sentence}
     return render(request, 'everest/sentence.html', context)
 
 @login_required
 @transaction.atomic
 def del_nepaliaudio(request, audio):
     audio = get_object_or_404(NepaliAudio, id=audio)
-    context = {'sentence': audio.sentence}
+    sentence = audio.sentence
     if request.method == 'POST' and request.user == audio.creator:
         audio.delete()
+        cleanup(sentence)
+    context = {'sentence': sentence}
     return render(request, 'everest/sentence.html', context)
 
 @login_required
 @transaction.atomic
 def del_sentence(request, sentence):
-    context = {'lesson': sentence.lesson}
-    return render(request, 'everest/sentence.html', context)
+    sentence = get_object_or_404(Sentence, id=sentence)
+    if request.method == 'POST' and request.user == sentence.creator:
+        # figure out which lesson this is. Delete sentence from its sentences. Put lesson in context
+        cleanup(sentence)
+    return render(request, 'everest/lesson.html', context)
 
 @login_required
 @transaction.atomic
 def del_lesson(request, lesson):
     context = {'lesson': lesson}
+    # cleanup all sentences in it
     return render(request, 'everest/sentence.html', context)
+
+def cleanup(sentence):
+    #todo
+    return
