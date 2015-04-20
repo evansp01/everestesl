8765c5ddc11dc4f4ceb429a67a9cd80ad075c968
Evan Palmer <evansp01@gmail.com>
2015-04-20 05:44:12 -0400

    fixed user_table template, added quotes around query string

--- everest/models.py
+++ everest/models.py
@@ -24,11 +24,11 @@ class UserProfile(models.Model):
     userkey = models.OneToOneField(User, related_name='profile')
     bio = models.CharField(max_length=430, blank=True)
     image = models.ImageField(upload_to="pictures", blank=True, null=True)
-    ESLTEACHER = 'E'
+    ESL_TEACHER = 'E'
     TRANSLATOR = 'T'
     OTHER = 'O'
     USER_TYPES = (
-        (ESLTEACHER, 'ESL_Teacher'),
+        (ESL_TEACHER, 'ESL_Teacher'),
         (TRANSLATOR, 'Translator'),
         (OTHER, 'Other')
     )
